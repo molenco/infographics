1a8a288d9f4ad052d5e3cc6dadf1d6876aec6da6
Juan Enrique Arango <juanenrique55@gmail.com>
2018-10-05 21:13:46 -0400

    add code from codepen

---
+++ js/images.js
@@ -0,0 +1,18 @@
+ $(document).ready(function() {
+    $('#zanahorias').hide();
+        $('#boton1').click(function() {
+            $('#zanahorias').show();
+   });
+     $('#gota').hide();
+        $('#boton2').click(function() {
+            $('#gota').show();
+   });
+      $('#tractor').hide();
+        $('#boton2').click(function() {
+            $('#tractor').show();
+   });
+      $('#keyline').hide();
+        $('#boton2').click(function() {
+            $('#keyline').show();
+   });
+ });
