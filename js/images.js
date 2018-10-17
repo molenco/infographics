407946f193949de1be1e5315c8cf2caf9826e370
Nick McCurdy <nick@nickmccurdy.com>
2018-10-17 00:33:41 -0400

    Replace jQuery with vanilla JS

--- js/images.js
+++ js/images.js
@@ -1,18 +1,29 @@
- $(document).ready(function() {
-    $('#zanahorias').hide();
-        $('#boton1').click(function() {
-            $('#zanahorias').show();
-   });
-     $('#gota').hide();
-        $('#boton2').click(function() {
-            $('#gota').show();
-   });
-      $('#tractor').hide();
-        $('#boton2').click(function() {
-            $('#tractor').show();
-   });
-      $('#keyline').hide();
-        $('#boton2').click(function() {
-            $('#keyline').show();
-   });
- });
+var boton1 = document.querySelector("#boton1");
+var boton2 = document.querySelector("#boton2");
+var gota = document.querySelector("#gota");
+var keyline = document.querySelector("#keyline");
+var tractor = document.querySelector("#tractor");
+var zanahorias = document.querySelector("#zanahorias");
+
+function hide(element) {
+  element.style.display = "none";
+}
+
+function show(element) {
+  element.style.display = "";
+}
+
+document.addEventListener("DOMContentLoaded", function() {
+  hide(zanahorias);
+  boton1.click();
+  show(zanahorias);
+  hide(gota);
+  boton2.click();
+  show(gota);
+  hide(tractor);
+  boton2.click();
+  show(tractor);
+  hide(keyline);
+  boton2.click();
+  show(keyline);
+});
